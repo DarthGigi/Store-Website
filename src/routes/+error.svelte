946cf21119270a1d81071f7353254ae577dc5768
Gigi <47110839+DarthGigi@users.noreply.github.com>
2023-06-16 18:24:45 +0200

    Better error handling and display

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,10 +1,21 @@
-<svelte:head>
-  <title>404</title>
-</svelte:head>
+<script>
+  import { page } from '$app/stores';
+</script>
 
+<svelte:head>
+  <title>
+    {$page.status}
+    {$page.error?.message}
+  </title>
+</svelte:head>
 <div class="flex h-screen w-screen items-center justify-center py-6 sm:py-8 lg:py-12">
   <div class="mx-auto max-w-screen-md px-4 text-[#979797] md:px-8">
-    <h1 class="mb-2 !font-sfdisplay text-2xl font-medium text-white sm:text-3xl md:text-5xl">404</h1>
+    <h1 class="mb-2 !font-sfdisplay text-2xl font-medium text-white sm:text-3xl md:text-5xl">
+      {$page.status}
+      {#if $page.error?.message}
+        — {$page.error?.message}
+      {/if}
+    </h1>
     <p class="mb-6 !font-sfdisplay sm:text-lg md:mb-8">
       The page you are looking for does not exist or something went wrong. <br /> Are you sure you are in the right place?
     </p>
