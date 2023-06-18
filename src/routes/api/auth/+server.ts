0239b9a22ae63996ee7fc7987804e4639718d9cd
Max <maxt@maxt.church>
2023-06-18 06:33:02 -0400

    Remove Server Join from permissions requested from Discord

--- src/routes/api/auth/+server.ts
+++ src/routes/api/auth/+server.ts
@@ -1,7 +1,7 @@
 import { PUBLIC_DISCORD_OAUTH_ID, PUBLIC_REDIRECT_URI } from '$env/static/public';
 import type { RequestHandler } from './$types';
 
-const DISCORD_ENDPOINT = `https://discord.com/oauth2/authorize?response_type=code&client_id=${PUBLIC_DISCORD_OAUTH_ID}&scope=connections%20guilds.join%20guilds.members.read%20identify&redirect_uri=${encodeURIComponent(PUBLIC_REDIRECT_URI)}&prompt=consent&state=decf3e511b40011b9`;
+const DISCORD_ENDPOINT = `https://discord.com/oauth2/authorize?response_type=code&client_id=${PUBLIC_DISCORD_OAUTH_ID}&scope=connections%20guilds.members.read%20identify&redirect_uri=${encodeURIComponent(PUBLIC_REDIRECT_URI)}&prompt=consent&state=decf3e511b40011b9`;
 
 export const GET: RequestHandler = async () => {
   return new Response(null, {
